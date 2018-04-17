84478763c207e132e9f8b016a400dadcd1cefd86
caseyyee <caseyyee.ca@gmail.com>
2018-04-17 08:33:21 -0700

    wip links

---
+++ Assets/WebVR/Scripts/Url.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Url : MonoBehaviour {
+    public string Location = "https://aframe.io/a-blast/";
+}
